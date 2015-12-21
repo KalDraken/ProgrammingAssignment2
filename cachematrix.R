71296139e29b34f856da884c40bba7970baf24dd
KalDraken <eacartwr@gmail.com>
2015-12-21 01:24:00 -0600

    Modified the original document to complete the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Does what it needs to do to complete the assignment I had to turn
+## to the wider internet to complete instead of the course that I paid
+## for as related to the lectures for the week.
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmatrix <- function(matrix) m <<- matrix
+  getmatrix <- function() m
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
 }
 
 
-## Write a short comment describing this function
+## Refers to variable by name in data frame with $. If M isn't NULL
+## returns M.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getmatrix()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- matrix(data, ...)
+  x$setmatrix(m)
+  m
 }
